ed2f077c6ff143f478b04e7a6e7f5545c6ed7878
Akash <2004akashchauhan@gmail.com>
2023-10-24 15:21:37 +0530

    .

--- test.py
+++ test.py
@@ -1,13 +1,3 @@
-from PIL import ImageGrab
-import numpy as np
-import cv2 
+import cv2
+print(cv2.__version__)
 
-
-while True:
-    img = ImageGrab.grab(bbox=(0, 0, 1280, 720))
-    img_np = np.array(img)
-    cv2.imshow('Screen Capture', img_np)
-    if cv2.waitKey(10) & 0xFF == ord('q'):
-        break
-
-cv2.destroyAllWindows()
